528d2527f9cc2c69f8b8398f764f0c0b0799fdaa
Ryan Scott <ryankennethscott@gmail.com>
2019-06-01 13:54:41 +1200

    Removing associating JS files with web-mode

--- init.el
+++ init.el
@@ -953,7 +953,6 @@
   (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.xml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.js[x]?\\'" . web-mode))
   )
 
 (use-package prettier-js
